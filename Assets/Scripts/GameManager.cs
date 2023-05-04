72d9c103b055239b141c223293391c7e68efa307
therealkylepacheco <kpacheco@nnu.edu>
2023-05-04 08:49:32 -0600

    Adding background effect

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject star;
+    public float backgroundRate = 0.25f;
+
+    public bool gameIsActive = true;
+
+    private float backgroundRange = 543;
+    private float backgroundX = 1280;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnStar());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    IEnumerator SpawnStar()
+    {
+        while (gameIsActive)
+        {
+            yield return new WaitForSeconds(backgroundRate);
+
+            float backgroundY = Random.Range(-1 * backgroundRange, backgroundRange);
+            Vector3 starPosition = new Vector3(backgroundX, backgroundY, star.transform.position.z);
+            Quaternion starRotation = star.transform.rotation;
+
+            Instantiate(star, starPosition, starRotation);
+        }
     }
 }
